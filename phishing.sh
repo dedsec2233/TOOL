e53222f9d9a1b5f27ee43e8986e588ec28091511
dedsec2233 <69725203+dedsec2233@users.noreply.github.com>
2020-08-16 06:57:14 +0700

    Create phishing.sh

---
+++ phishing.sh
@@ -0,0 +1,7 @@
+echo "# shellphish" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:dedsec2233/shellphish.git
+git push -u origin master
+                
